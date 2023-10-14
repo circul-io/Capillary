58ec2df5287838afd26f0aaa9c1229e8de685730
Jeremy Willemse <jeremy@willemse.co>
2023-10-14 13:12:34 -0700

    add gradle plugins for docs, signing, publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
 plugins {
     kotlin("multiplatform") version "1.9.0"
+    id("org.jetbrains.dokka") version "1.9.0"
+    id("maven-publish")
+    id("signing")
 }
 
 group = "io.circul"
-version = "0.1.0-alpha"
+version = "1.0.0-alpha1"
 
 repositories {
     mavenCentral()
@@ -42,18 +45,67 @@ kotlin {
 
     
     sourceSets {
-        val commonMain by getting
-        val commonTest by getting {
+        getByName("commonMain")
+        getByName("commonTest") {
             dependencies {
                 implementation(kotlin("test"))
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
             }
         }
-        val jvmMain by getting
-        val jvmTest by getting
-        val jsMain by getting
-        val jsTest by getting
-        val nativeMain by getting
-        val nativeTest by getting
     }
+}
+
+tasks.register<Jar>("javadocJar") {
+    archiveClassifier.set("javadoc")
+}
+
+publishing {
+    publications {
+        named<MavenPublication>("jvm") {
+            artifact(tasks["javadocJar"])
+        }
+
+        withType<MavenPublication>().all {
+            pom {
+                name.set("Capillary")
+                description.set("Ports and Adapters Interface Library")
+                url.set("https://capillary.circul.io")
+
+                licenses {
+                    license {
+                        name.set("The Apache Software License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("JeremyMWillemse")
+                        name.set("Jeremy Willemse")
+                        email.set("jeremy@willemse.co")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/circul-io/Capillary")
+                    connection.set("scm:git:https://github.com/circul-io/Capillary.git")
+                    developerConnection.set("scm:git:ssh://github.com/circul-io/Capillary.git")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "OSSRH"
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+
+            credentials {
+                username = project.findProperty("ossrhUsername") as String?
+                password = project.findProperty("ossrhPassword") as String?
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications)
 }
